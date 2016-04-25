4b5c879ad2e0b49dc60aab3444df9ee27db951b4
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-04-25 17:11:49 +0100

    changed pull request comments and added nicer spec layout

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,5 +1,16 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['e2e/*.js'],
-  baseUrl: 'http://localhost:8080'
+  baseUrl: 'http://localhost:8080',
+
+ onPrepare: function() {
+  var SpecReporter = require('jasmine-spec-reporter');
+  jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+  },
+
+  jasmineNodeOpts: {
+   print: function() {}
+  }
 };
+
+
