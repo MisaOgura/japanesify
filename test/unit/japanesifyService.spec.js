415029a26921ddcd341f08a219948e640db1cf9b
Misa Ogura <misa.ogura01@gmail.com>
2016-04-26 12:03:01 +0100

    Unit test for conversion function in japanesify passing

--- test/unit/japanesifyService.spec.js
+++ test/unit/japanesifyService.spec.js
@@ -1,16 +1,20 @@
 describe('japenesifyService', function (){
   beforeEach(module('japanesifyApp'));
 
-  var japenesifyService, JPrule, string;
+  var japenesifyService, string;
+  var ruleJP = {regex: /([b-df-hj-np-tv-z][aeiou]r|[b-df-hj-np-tv-z][aeiou]|[b-df-hj-np-tv-z])/i, matcher: { "Ca": "カ", 's': 'ス', 'par': 'パー' }};
 
   beforeEach(inject(function(_japanesifyService_){
-    japenesifyService = _japanesifyService_
-    var JPrule = {regex: "/[b-df-hj-np-tv-z][eaiou]/i", matcher: {"/a/":"あ"}}
-    var string = "Caspar"
+    japenesifyService = _japanesifyService_;
   }));
 
   it ('split string based on regex', function() {
-    expect(japenesifyService.splitIntoSyllables("Caspar", {regex: /([b-df-hj-np-tv-z][aeiou]r|[b-df-hj-np-tv-z][aeiou]|[b-df-hj-np-tv-z])/i, matcher: {"/a/":"あ"}})).toEqual(["Ca","s","par"])
-  })
+    var string = "Caspar";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["Ca","s","par"]);
+  });
 
+  it ('convert an array of syllables to a japanese string', function() {
+    var array = ["Ca","s","par"];
+    expect(japenesifyService.convertToJapanese(array, ruleJP)).toEqual('カスパー');
+  });
 });
