c1965b2d568f188bf551453b33453c4224036fd9
Misa Ogura <misa.ogura01@gmail.com>
2016-04-26 16:54:22 +0100

    Hash on the way

--- app/js/rulesFactory.js
+++ app/js/rulesFactory.js
@@ -1,8 +1,23 @@
 japanesifyApp.factory('RulesFactory', function() {
   function RulesFactory() {
-    this.regex = {};
-    this.matcher = [];
-  };
+    this.regex = /(ll)[aeiou]|[b-df-hj-np-tv-z][aeiou](r|w)\b|(phoe)|([b-df-hj-np-tv-z](i))(?!(e))|[b-df-hj-np-tv-z][aeiou]{2}|[b-df-hj-np-tv-z](h)[aeiou]|(nn)[aeiou]|(ch)(?![aeiou])|(ch)[aeiou]|[b-df-hj-np-tv-z][aeiou]|[b-df-hj-np-tv-z]|[aeiou]/i;
+    this.matcher = {'e': 'え',
+                    'ri': 'り',
+                    'ka': 'か',
+                    'mi':  '',
+                    'sa':  '',
+                    'mu': '',
+                    'lo': '',
+                    're': '',
+                    'n': '',
+                    'zo': '',
+                    'to': '',
+                    'be': '',
+                    'nna': '',
+                    'ke': '',
+                    'vi': '',
+                    };
+  }
 
   return RulesFactory;
 });
