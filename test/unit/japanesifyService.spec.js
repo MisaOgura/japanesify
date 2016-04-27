fd6c107be7df89373d2ef583e19feced84f6113f
Misa Ogura <misa.ogura01@gmail.com>
2016-04-27 09:03:58 +0100

    Add unit tests to cover more rules

--- test/unit/japanesifyService.spec.js
+++ test/unit/japanesifyService.spec.js
@@ -59,28 +59,58 @@ describe('japenesifyService', function (){
     japenesifyService = _japanesifyService_;
   }));
 
-  it ('split string based on regex', function() {
+  it ('combines a vowel with a preceding consonant', function() {
+    var string = "misa";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["mi","sa"]);
+  });
+
+  it ("slices an 'n' out if it precedes a consonant", function() {
+    var string = "lorenzo";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["lo","re","n", "zo"]);
+  });
+
+  it ("slices an 'n' out if it's the last letter", function() {
+    var string = "kevin";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["ke","vi","n"]);
+  });
+
+  it ("combines a double 'n' and a following vowel", function() {
+    var string = "tobenna";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["to","be","nna"]);
+  });
+
+  it ("slices an 's' out it precedes a consonant", function() {
+    var string = "yasmin";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["ya","s","mi", "n"]);
+  });
+
+  it ("combines the last 'r' with a preceding vowel or contonant/vowel pair", function() {
     var string = "caspar";
     expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["ca","s","par"]);
+  });
+
+  it ("recognises and split 'ch' followed by a consonant", function() {
+    var string = "chris";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["ch","ri","s"]);
+  });
+
+  it ("recognises 'sh' and combines it with a following vowel", function() {
+    var string = "shane";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["sha","ne"]);
+  });
+
+  it ("splits a consonant if it's the last letter, expecpt for 'r'", function() {
+    var string = "adil";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["a","di","l"]);
+  });
+
+  it ("recognise and split 'ph' followed by a double consonant", function() {
+    var string = "phoebe";
+    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["phoe","be"]);
   });
 
   it ('convert an array of syllables to a japanese string', function() {
     var array = ["ca","s","par"];
     expect(japenesifyService.convertToJapanese(array, ruleJP)).toEqual('カスパー');
-  });
-
-  it ("recognise double 'nn's followed by a vowel", function() {
-    var string = "tobenna";
-    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["to","be","nna"]);
-  });
-
-  it ("recognise and split 'ch' followed by a consonant", function() {
-    var string = "chris";
-    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["ch","ri","s"]);
-  });
-
-  it ("recognise and split 'ch' followed by a consonant", function() {
-    var string = "phoebe";
-    expect(japenesifyService.splitIntoSyllables(string, ruleJP)).toEqual(["phoe","be"]);
   });
 });
