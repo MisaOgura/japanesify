323914bc01aafbb26ab2fd4b0325166bbad3ce81
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-28 17:57:16 +0100

    link to print next to each name

--- app/js/japanesifyController.js
+++ app/js/japanesifyController.js
@@ -11,4 +11,12 @@ japanesifyApp.controller('japanesifyController', ['TranslationFactory', 'japanes
     return new TranslationFactory(name);
   };
 
+  vm.printId = function(id) {
+    var id = "translation-" + id;
+    var printContents = document.getElementById(id).innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+  };
 }]);
