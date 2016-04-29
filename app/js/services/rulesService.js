daabbc6b1b356cd573401f585264733c6ad8cf86
Misa Ogura <misa.ogura01@gmail.com>
2016-04-29 11:25:16 +0100

    refactor regex, add hat to each regex

--- app/js/services/rulesService.js
+++ app/js/services/rulesService.js
@@ -4,30 +4,29 @@ japanesifyApp.service('rulesService', ['CONVERSION_CONSTANT', function(CONVERSIO
     return CONVERSION_CONSTANT;
   };
 
-  var consVowel = '[b-df-hj-np-tv-z][aeiou]';
-  var doubleCons = '[b-df-hj-np-tv-z]{2}\\b';
-  var startsCHCons = '\\bch(?![aeiou])';
-  var consHVowel = '[b-df-hj-np-tv-z]h[aeiou]';
-  var consDoubleVowel = '[bdf-hj-np-tv-z][aeo][aeu]';
-  var consVowelRWY = '[b-df-hj-np-tv-z][aeiou][rwy](\\b|(?![aeiouy]))';
-  var doubleLNVowel = '[ln]{2}[aeiou]';
-  var doubleConsDoubleVowel = '[bdf-hj-np-tv-z]{2}[aeo][aeiu]';
-
+  var consVowel = '^[b-df-hj-np-tv-z][aeiou]';
+  var doubleCons = '^[b-df-hj-np-tv-z]{2}\\b';
+  var startsCHCons = '^ch(?![aeiou])';
+  var consHVowel = '^[b-df-hj-np-tv-z]h[aeiou]';
+  var consDoubleVowel = '^[bdf-hj-np-tv-z][aeo][aeu]';
+  var consVowelRWY = '^[b-df-hj-np-tv-z][aeiou][rwy](\\b|(?![aeiouy]))';
+  var doubleLNVowel = '^[ln]{2}[aeiou]';
+  var doubleConsDoubleVowel = '^[bdf-hj-np-tv-z]{2}[aeo][aeiu]';
 
   this.twoCharSyllables = function(name){
-    return new RegExp('^'+'('+consVowel+'|'+
-                              doubleCons+'|'+
-                              startsCHCons+')', 'i');
+    return new RegExp(consVowel+'|'+
+                      doubleCons+'|'+
+                      startsCHCons, 'i');
   };
 
   this.threeCharSyllables = function(name){
-    return new RegExp('^'+'('+consHVowel+'|'+
-                              consDoubleVowel+'|'+
-                              consVowelRWY+'|'+
-                              doubleLNVowel+')', 'i');
+    return new RegExp(consHVowel+'|'+
+                      consDoubleVowel+'|'+
+                      consVowelRWY+'|'+
+                      doubleLNVowel, 'i');
   };
 
   this.fourCharSyllables = function(name) {
-    return new RegExp('^'+'('+doubleConsDoubleVowel+')', 'i');
+    return new RegExp(doubleConsDoubleVowel, 'i');
   };
 }]);
